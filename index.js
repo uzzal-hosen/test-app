62b16ac5ef2b14a28e6a472ed4d6203c9cf26702
Uzzal Hosen <uzzalh.me@gmail.com>
2023-05-23 11:45:32 +0600

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+const port = 3900;
+
+// Middleware to log incoming requests and headers
+app.use((req, res, next) => {
+    console.log(`Received ${req.method} request for ${req.url}`);
+    console.log('Request Headers:', req.headers);
+    next();
+});
+
+// Route to handle the root endpoint
+app.get('/', (req, res) => {
+    const response = {
+        message: 'Welcome to the Express app!',
+    };
+    res.json(response);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
